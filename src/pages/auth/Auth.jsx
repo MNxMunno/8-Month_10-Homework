f2af3b61ae6cffddd304ee2a1ab5f42c2f901a68
MNxMunno <bilolhuzayfa@gmail.com>
2024-06-29 12:11:19 +0500

    change

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const Auth = () => {
-  let isLogin = localStorage.getItem("x-auth-token");
+  let isLogin = useSelector((s) => s.auth.token); // localStorage.getItem("x-auth-token");
   return isLogin ? <Outlet /> : <Navigate replace to={"/login"} />;
 };
 
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSignInMutation } from "../../context/api/userApi";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setToken, setUser } from "../../context/slice/authSlice";
 
 const Login = () => {
   const [signIn, { data, isLoading, isSuccess }] = useSignInMutation();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(setToken(data?.data?.token));
+      dispatch(setUser(data?.data?.user));
+      navigate("/admin");
+    }
+  });
   const handleLogin = () => {
     signIn({ UserName: "john32", password: "12345678" });
+
     navigate("/admin");
   };
 
